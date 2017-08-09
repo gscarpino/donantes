71f4c201029c7f6da6ea08cbfc24cac887cac5b1
Gino Scarpino <gino.scarpino@gmail.com>
2017-08-09 05:55:14 +0000

    .

--- ses.js
+++ ses.js
@@ -1,5 +1,5 @@
-var k = "AKIAJ35YNBNMOH5A25OQ";
-var s = "P0/uB9By8L8bV8jZ86aqy3afqGM349Hh2HkBvadP";
+var k = "";
+var s = "";
 
 var ses = require('node-ses')
   , client = ses.createClient({ key: k, secret: s , amazon: 'https://email.us-west-2.amazonaws.com'});
