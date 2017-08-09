dc1bb7afe8f0af820afc80001705b2111dd09e1a
Gino Scarpino <gino.scarpino@gmail.com>
2017-08-09 05:59:00 +0000

    .

--- ses.js
+++ ses.js
@@ -1,13 +1,8 @@
-var k = "AKIAJ35YNBNMOH5A25OQ";
-var s = "P0/uB9By8L8bV8jZ86aqy3afqGM349Hh2HkBvadP";
 
-var ses = require('node-ses')
-  , client = ses.createClient({ key: k, secret: s , amazon: 'https://email.us-west-2.amazonaws.com'});
 
 client.sendEmail({
  to: 'gino.scarpino@gmail.com'
  , from: 'donantes.voluntarios.sangre@gmail.com'
- , subject: 'Prueba de Amazon SES'
  , message: 'Hola! <b>yes</b> vamoooo los pibe'
  , altText: 'hola'
 }, function (err, data, res) {
